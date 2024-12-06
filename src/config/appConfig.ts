91a3e84927b9de48e0787c2b478691314bd03ef3
Ashutosh Jha <ashutoshj887@gmail.com>
2024-12-07 01:29:19 +0530

    feat: fix configs

---
+++ src/config/appConfig.ts
@@ -0,0 +1,7 @@
+const appConfig = {
+  title: "ETHSapien",
+  description:
+    "Decentralized AI-Powered Credentialing System for On-Chain Identity Verification and Reputation",
+};
+
+export default appConfig;
