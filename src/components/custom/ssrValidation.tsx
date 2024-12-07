dbed46be842dccdc73a033ffbb706e0ee771239b
Ashutosh Jha <ashutoshj887@gmail.com>
2024-12-07 22:46:26 +0530

    fix: imports

--- src/components/custom/ssrValidation.tsx
+++ src/components/custom/ssrValidation.tsx
@@ -1,6 +1,7 @@
+import { GetServerSidePropsContext } from "next";
 import { getSession } from "next-auth/react";
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getSession(context);
 
   if (!session) {
@@ -12,12 +13,12 @@ export async function getServerSideProps(context) {
     };
   }
 
-  // Pass authenticated session data to the page
   return {
     props: { session },
   };
 }
 
-export default function Dashboard({ session }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function Dashboard({ session }: { session: any }) {
   return <h1>Authenticated as {session.user.email}</h1>;
 }
--- src/configs/SignOutButton.tsx
+++
@@ -1,5 +0,0 @@
-import { signOut } from "next-auth/react";
-
-export default function SignOutButton() {
-  return <button onClick={() => signOut()}>Sign Out</button>;
-}
